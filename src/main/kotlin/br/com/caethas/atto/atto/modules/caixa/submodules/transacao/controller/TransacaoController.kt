4c34fc4a21df072baee9b3b646ad05f2fe4d27c4
caethanus <machadocaetano@rede.ulbra.br>
2026-03-24 09:21:17 -0300

    quase finalizando transacao

--- src/main/kotlin/br/com/caethas/atto/atto/modules/caixa/submodules/transacao/controller/TransacaoController.kt
+++ src/main/kotlin/br/com/caethas/atto/atto/modules/caixa/submodules/transacao/controller/TransacaoController.kt
@@ -1,8 +1,8 @@
 package br.com.caethas.atto.atto.modules.caixa.submodules.transacao.controller
 
-import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.dto.TransacaoFromDto
-import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.dto.TransacaoToDto
+import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.dto.TransacaoDto
 import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.entity.TransacaoEntity
+import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.repository.TransacaoRepository
 import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.service.TransacaoService
 import br.com.caethas.atto.atto.shared.base.BaseController
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("transacoes")
 class TransacaoController(service: TransacaoService) :
-    BaseController<TransacaoEntity, TransacaoFromDto, TransacaoToDto>(
+    BaseController<TransacaoEntity, TransacaoRepository, TransacaoDto>(
         service
     ) {
 }
