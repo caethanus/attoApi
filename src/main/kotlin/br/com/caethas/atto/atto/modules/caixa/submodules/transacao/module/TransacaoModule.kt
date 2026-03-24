8214f25ba213229818d6622038ccf2814978ceb1
caethanus <machadocaetano@rede.ulbra.br>
2026-03-24 15:54:02 -0300

    Update TransacaoModule.kt

--- src/main/kotlin/br/com/caethas/atto/atto/modules/caixa/submodules/transacao/module/TransacaoModule.kt
+++ src/main/kotlin/br/com/caethas/atto/atto/modules/caixa/submodules/transacao/module/TransacaoModule.kt
@@ -1,6 +1,7 @@
 package br.com.caethas.atto.atto.modules.caixa.submodules.transacao.module
 
 import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.mapper.TransacaoMapper
+import br.com.caethas.atto.atto.modules.caixa.submodules.transacao.service.TransacaoService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,5 +11,13 @@ class TransacaoModule {
     @Bean
     fun transacaoMapper(): TransacaoMapper {
         return TransacaoMapper()
+    }
+    
+    @Bean
+    fun transacaoService() : TransacaoService {
+        return TransacaoService(
+            repository = ,
+            mapper = 
+        )
     }
 }
